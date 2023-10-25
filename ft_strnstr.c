1fcd9ac6686060f9347ac5c0e99eeec55c033c38
Jonathan Albiser <jalbiser@k2r4p4.42mulhouse.fr>
2023-10-25 13:11:27 +0200

    updateee

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jalbiser <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/23 06:19:14 by jalbiser          #+#    #+#             */
-/*   Updated: 2023/10/23 06:19:37 by jalbiser         ###   ########.fr       */
+/*   Created: 2023/10/25 13:10:43 by jalbiser          #+#    #+#             */
+/*   Updated: 2023/10/25 13:10:47 by jalbiser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -17,21 +17,17 @@ char	*ft_strnstr(const char *a, const char *b, size_t n)
 	size_t	c;
 
 	i = 0;
-	c = 0;
-	if ((*b == '\0') || (*a == '\0'))
+	if (*b == '\0')
 		return ((char *)a);
-	while (i < n)
+	while ((a[i] != '\0') && (i < n))
 	{
-		while (a[i] == b[c])
-		{
-			if (i >= n)
-				return (NULL);
-			if (b[c] == '\0')
-				return ((char *)&a[i - c]);
-			c++;
-			i++;
-		}
 		c = 0;
+		while ((a[i + c] == b[c]) && ((i + c) < n))
+		{
+			if (b[c + 1] == '\0')
+				return ((char *)&a[i]);
+			c++;
+		}
 		i++;
 	}
 	return (NULL);
