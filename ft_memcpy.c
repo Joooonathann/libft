3fbc2eb5ee7965027e89244fc63e14504df31da9
Jonathan Albiser <jalbiser@k2r4p4.42mulhouse.fr>
2023-10-25 11:21:11 +0200

    updatee

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: jalbiser <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:29:37 by jalbiser          #+#    #+#             */
-/*   Updated: 2023/10/25 11:18:49 by jalbiser         ###   ########.fr       */
+/*   Updated: 2023/10/25 11:21:05 by jalbiser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -20,6 +20,8 @@ void	*ft_memcpy(void *dest, const void *source, size_t size)
 	a = (char *) dest;
 	b = (char *) source;
 	y = 0;
+	if (!a && !b)
+		return (NULL);
 	while (y < size)
 	{
 		a[y] = b[y];
