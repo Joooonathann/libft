05da032a2328fdc8e3d67e8d1bbd3de169c529e3
Peliium68 <peliium.2022@gmail.com>
2023-10-26 22:36:36 +0200

    fix

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -12,20 +12,6 @@
 #include "libft.h"
 #include <stdlib.h>
 
-static int	ft_is_set_char(char c, char const *set)
-{
-	size_t	i;
-
-	i = 0;
-	while (set[i] != '\0')
-	{
-		if (c == set[i])
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	size_t	start;
@@ -35,10 +21,10 @@ char	*ft_strtrim(char const *s1, char const *set)
 	if (!s1)
 		return (NULL);
 	start = 0;
-	while (s1[start] && ft_is_set_char(s1[start], set))
+	while (s1[start] && ft_strchr(set, s1[start]))
 		start++;
 	end = ft_strlen(s1);
-	while (end > start && ft_is_set_char(s1[end - 1], set))
+	while (end > start && ft_strchr(set, s1[end - 1]))
 		end--;
 	str = (char *)malloc(sizeof(char) * (end - start + 1));
 	if (!str)
